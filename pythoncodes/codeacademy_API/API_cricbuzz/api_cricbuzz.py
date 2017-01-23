1b7d01f0bffde2c5c44f9bb24415ef91e08c25ac
shisuiUchiha <santhukota1996@gmail.com>
2017-01-24 00:38:48 +0530

    add

--- pythoncodes/codeacademy_API/API_cricbuzz/api_cricbuzz.py
+++ pythoncodes/codeacademy_API/API_cricbuzz/api_cricbuzz.py
@@ -2,31 +2,46 @@ import requests
 import xml.etree.ElementTree as ET 
 
 
-response=requests.get('http://synd.cricbuzz.com/j2me/1.0/livematches.xml')
-
-head=response.headers
-
-data=response.text
-
-print data
-
-root=ET.fromstring(data)
+def get_url(url):
+	response=requests.get(url)
+	head=response.headers
+	data=response.text
+	return data
 
 
+def matches_today(xml_data):
+	root=ET.fromstring(xml_data)
+	#print "Number of Matches held today are " + root.attrib['NMchs']
+	print "Matches which are going now or completed recently"
+	for child in root:
+		if(child.tag=="match"):
+			print child.attrib['mchDesc']+" <======  ID  OF   THE   MATCH  =====>  " + child.attrib['id']
+	return root
+
+
+def match_watch(get_root,match_id):
+	root=get_root
+	Id=match_id
+	#print Id
+	#print type(Id)
+	if (int(Id)<10000):
+		for child in root:
+			if (child.tag=="match"):
+				if(child.attrib['id']==Id):
+					return child
+	else:
+		print "Number which is less than 10000"
+		Id=raw_input("Select the Match ID once again from above Description - ")
+		return match_watch(root,Id)
+		#This is to be done later
 
 
 
-for child in root:
-	print child.tag
-	d={}
-	d=child.attrib
-	print d
-	for keys in child.attrib:
-		if (child.attrib[keys]=="BANW vs RSAW"):
-			for childs in child:
-				if (childs.tag=="state"):
-					for things in childs.attrib:
-						if(things=="status"):
-							print childs.attrib[things]
+url="http://synd.cricbuzz.com/j2me/1.0/livematches.xml"
+xml_data=get_url(url)
+got_root=matches_today(xml_data)
+match_id=raw_input("Select the Match ID from above Description - ")
+second_root=match_watch(got_root,match_id)
+print second_root.attrib['id']
 
 
